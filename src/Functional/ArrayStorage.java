2f282687e0c86f57ca1992bd3b093f199aa08b33
VHR <just_vhr@mail.ru>
2025-01-24 17:50:53 +0700

    Added realisation of getAll method

--- src/Functional/ArrayStorage.java
+++ src/Functional/ArrayStorage.java
@@ -4,26 +4,27 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     private static int counterOfResume = 0;
+
     void clear() {
     }
 
     void save(Resume r) {
-        if(counterOfResume!=10000){
+        if (counterOfResume != 10000) {
             this.storage[counterOfResume] = r;
             counterOfResume++;
         }
     }
 
     Resume get(String uuid) {
-        int foundID=indexOfResume(uuid);
+        int foundID = indexOfResume(uuid);
         if (size() == 0) return null;
-        if(foundID!=-1){
+        if (foundID != -1) {
             return this.storage[foundID];
         }
         return null;
     }
 
-    int indexOfResume(String uuid){
+    int indexOfResume(String uuid) {
         int i = 0;
         for (; i < size(); i++) {
             if (uuid.equals(this.storage[i].toString())) {
@@ -32,10 +33,11 @@ public class ArrayStorage {
         }
         return -1;
     }
+
     void delete(String uuid) {
-        int foundID=indexOfResume(uuid);
-        if(foundID!=-1){
-            this.storage[foundID]=null;
+        int foundID = indexOfResume(uuid);
+        if (foundID != -1) {
+            this.storage[foundID] = null;
             for (int i = foundID; i < size(); i++) {
                 if ((this.storage[i] == null) && (this.storage[i + 1] != null)) {
                     Resume tmp = this.storage[i];
@@ -51,7 +53,14 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        if (counterOfResume == 0) {
+            return null;
+        }
+        Resume[] outputAll = new Resume[counterOfResume];
+        for (int i = 0; i < size(); i++) {
+            outputAll[i] = this.storage[i];
+        }
+        return outputAll;
     }
 
     int size() {
