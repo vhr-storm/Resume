1e20cd836397afc7253d52cc7daa77adcce3f19d
young_vhr <blackrabbit154@gmail.com>
2025-01-27 17:12:10 +0700

    Replaced all methods to AbstractArrayStorage

--- src/Functional/ArrayStorage.java
+++ src/Functional/ArrayStorage.java
@@ -1,7 +1,24 @@
 package Functional;
 
 public class ArrayStorage extends AbstractArrayStorage{
+    @Override
+    protected void insertElement(Resume r, int index) {
 
+    }
+
+    @Override
+    protected void fillDeletedElement(int index) {
+
+    }
+
+    public int getIndex(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
+    }
     /**
      * @return array, contains only Resumes in storage (without null)
      */
