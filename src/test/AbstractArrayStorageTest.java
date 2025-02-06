2e4a66e1e0c2b9bfe8def2eee934c7d2e51c912e
young_vhr <blackrabbit154@gmail.com>
2025-02-06 18:31:59 +0700

    Folder test removed to src

---
+++ src/test/AbstractArrayStorageTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.jupiter.api.*;
+
+class AbstractArrayStorageTest {
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void update() {
+    }
+}
---
+++ src/test/SortedArrayStorageTest.java
@@ -0,0 +1,4 @@
+package test;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest{
+}
