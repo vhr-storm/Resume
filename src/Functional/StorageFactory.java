7af48e54744d1387f839cf798df3ce3c1bf1ed1c
young_vhr <blackrabbit154@gmail.com>
2025-02-06 18:04:44 +0700

    Added factory method for arrays

--- src/Functional/StorageFactory.java
+++ src/Functional/StorageFactory.java
@@ -1,2 +1,19 @@
-package Functional;public class StorageFactory {
+package Functional;
+import Exception.*;
+
+public class StorageFactory {
+    public enum StorageType{
+        SORTED_ARRAY_STORAGE,
+        ARRAY_STORAGE,
+        NO_TYPE
+    }
+    public static   AbstractArrayStorage createStorage(StorageType type) {
+        AbstractArrayStorage typeStorage = null;
+        switch (type){
+            case ARRAY_STORAGE -> typeStorage = new ArrayStorage();
+            case SORTED_ARRAY_STORAGE -> typeStorage = new SortedArrayStorage();
+            default -> throw new NotExistTypeStorageException(type.toString());
+        }
+        return typeStorage;
+    }
 }
