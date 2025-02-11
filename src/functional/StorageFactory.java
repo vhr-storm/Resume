019c4e6c99bf39e634c26c152940017e8cebe768
young_vhr <blackrabbit154@gmail.com>
2025-02-11 21:38:40 +0700

    Reworked with new types of storage

--- src/functional/StorageFactory.java
+++ src/functional/StorageFactory.java
@@ -6,14 +6,18 @@ public class StorageFactory {
     public enum StorageType {
         SORTED_ARRAY_STORAGE,
         ARRAY_STORAGE,
+        MAP_STORAGE,
+        LIST_STORAGE,
         NO_TYPE
     }
 
-    public static AbstractArrayStorage createStorage(StorageType type) {
-        AbstractArrayStorage typeStorage = null;
+    public static AbstractStorage createStorage(StorageType type) {
+        AbstractStorage typeStorage = null;
         switch (type) {
             case ARRAY_STORAGE -> typeStorage = new ArrayStorage();
             case SORTED_ARRAY_STORAGE -> typeStorage = new SortedArrayStorage();
+            case MAP_STORAGE -> typeStorage = new MapStorage();
+            case LIST_STORAGE -> typeStorage = new ListStorage();
             default -> throw new NotExistTypeStorageException(type.toString());
         }
         return typeStorage;
