09d42567d8fa23f8f0be47fe366196016551e7be
young_vhr <blackrabbit154@gmail.com>
2025-02-11 17:44:17 +0700

    deleted unused comments

--- src/functional/ArrayStorage.java
+++ src/functional/ArrayStorage.java
@@ -13,17 +13,14 @@ public class ArrayStorage extends AbstractArrayStorage {
         storage[index] = storage[counterOfResume - 1];
     }
 
+    @Override
     public int getIndex(String uuid) {
         for (int i = 0; i < size(); i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
+
         return -1;
     }
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-
-
 }
