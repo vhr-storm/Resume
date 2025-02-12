8fef2838d6590692d1efa9cb7482c953f40a51d3
young_vhr <blackrabbit154@gmail.com>
2025-02-12 16:40:33 +0700

    Added update() implements

--- src/functional/ListStorage.java
+++ src/functional/ListStorage.java
@@ -8,17 +8,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ListStorage extends AbstractStorage{
+public class ListStorage extends AbstractStorage {
     static final List<Resume> RESUME_LIST = new ArrayList<Resume>();
+
     @Override
     public void clear() {
-        RESUME_LIST.clear();
+
+        if (!RESUME_LIST.isEmpty()) {
+            RESUME_LIST.clear();
+        }
+
     }
 
     @Override
     public void save(Resume r) throws ExistStorageException {
 
-        if(RESUME_LIST.contains(r)){
+        if (RESUME_LIST.contains(r)) {
             throw new ExistStorageException(r.getUuid());
         } else {
             RESUME_LIST.add(r);
@@ -27,33 +32,42 @@ public class ListStorage extends AbstractStorage{
     }
 
     @Override
-    public Resume get(String uuid)  {
+    public Resume get(String uuid) {
         Resume r = new Resume(uuid);
 
-        if(!RESUME_LIST.contains(r)){
+        if (!RESUME_LIST.contains(r)) {
             throw new NotExistStorageException(uuid);
         }
+
         return r;
     }
 
     @Override
     public Resume[] getAll() {
-        return Arrays.copyOfRange(RESUME_LIST.toArray(new Resume[0]),0,size());
+        return Arrays.copyOfRange(RESUME_LIST.toArray(new Resume[0]), 0, size());
     }
 
     @Override
     public void delete(String uuid) {
         Resume r = new Resume(uuid);
 
-        if(!RESUME_LIST.contains(r)){
+        if (!RESUME_LIST.contains(r)) {
             throw new NotExistStorageException(uuid);
         } else {
             RESUME_LIST.remove(r);
         }
+
     }
 
     @Override
-    public void update(Resume r) {
+    public void update(Resume r) throws NotExistStorageException {
+        int index = getIndex(r.getUuid());
+
+        if (!RESUME_LIST.contains(r)) {
+            throw new NotExistStorageException(r.getUuid());
+        } else {
+            RESUME_LIST.set(index, r);
+        }
 
     }
 
@@ -62,4 +76,8 @@ public class ListStorage extends AbstractStorage{
         return RESUME_LIST.size();
     }
 
+
+    public int getIndex(String uuid) {
+        return RESUME_LIST.indexOf(new Resume(uuid));
+    }
 }
