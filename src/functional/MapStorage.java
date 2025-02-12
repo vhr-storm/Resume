c1bffad69078efd693123406b33f9629e5287a20
young_vhr <blackrabbit154@gmail.com>
2025-02-12 16:50:51 +0700

    Added clear() implements

--- src/functional/MapStorage.java
+++ src/functional/MapStorage.java
@@ -1,4 +1,50 @@
 package functional;
 
+import model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapStorage extends AbstractStorage{
+
+    private final Map<String,Resume> MAP_RESUME = new HashMap<>();
+
+    @Override
+    public void clear() {
+
+        if(!MAP_RESUME.isEmpty()){
+           MAP_RESUME.clear();
+        }
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return null;
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
