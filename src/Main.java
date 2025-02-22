92023126218144a324326d9bbe4e0bd0c248b3b3
young_vhr <blackrabbit154@gmail.com>
2025-02-22 13:49:33 +0700

    reworked

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,46 @@
-import functional.AbstractArrayStorage;
+import functional.AbstractStorage;
 import functional.StorageFactory;
 import model.Resume;
 
 public class Main {
 
-    static AbstractArrayStorage arraySortedStorage = StorageFactory.createStorage(StorageFactory.StorageType.SORTED_ARRAY_STORAGE);
-
+    static AbstractStorage arraySortedStorage = StorageFactory.createStorage(StorageFactory.StorageType.ARRAY_STORAGE);
+    protected static final Resume[] TEST_RESUMES = {
+            new Resume("uuid10","Roman"),
+            new Resume("uuid11","Evgeniy"),
+            new Resume("uuid15","Alina"),
+            new Resume("uuid14","Natalia"),
+            new Resume("uuid9","Algrgey"),
+    };
     public static void main(String[] args) {
-        Resume r1 = new Resume("uuid3");
-        Resume r2 = new Resume("uuid1");
-        Resume r3 = new Resume("uuid5");
-        Resume r4 = new Resume("uuid4");
 
-        arraySortedStorage.save(r1);
-        arraySortedStorage.save(r2);
-        arraySortedStorage.save(r3);
-        arraySortedStorage.save(r4);
-        arraySortedStorage.save(r1);
-        System.out.println("Get r1: " + arraySortedStorage.get(r1.getUuid()));
-        System.out.println("Get r2: " + arraySortedStorage.get(r2.getUuid()));
+        arraySortedStorage.save(TEST_RESUMES[0]);
+
+        arraySortedStorage.save(TEST_RESUMES[1]);
+        arraySortedStorage.save(TEST_RESUMES[2]);
+        arraySortedStorage.save(TEST_RESUMES[3]);
+        arraySortedStorage.save(TEST_RESUMES[4]);
+//        arraySortedStorage.save(r1);
+
+        System.out.println("Get r1: " + arraySortedStorage.get(TEST_RESUMES[0].getUuid()));
 
         System.out.println("Size: " + arraySortedStorage.size());
 
         printAll();
-        arraySortedStorage.delete(r1.getUuid());
-        printAll();
-        arraySortedStorage.clear();
+        arraySortedStorage.delete(TEST_RESUMES[0].getUuid());
 //        arraySortedStorage.delete(r1.getUuid());
         printAll();
+//        arraySortedStorage.clear();
+        printAll();
         System.out.println();
-//        System.out.println("Size: " + arraySortedStorage.size());
-//        System.out.println(); arraySortedStorage.update(r4);
+        System.out.println("Size: " + arraySortedStorage.size());
+        arraySortedStorage.update(TEST_RESUMES[3]);
+        System.out.println("Get r4: " + arraySortedStorage.get(TEST_RESUMES[3].getUuid()));
     }
 
     static void printAll() {
         System.out.println("\nGet All");
-        for (Resume r : arraySortedStorage.getAll()) {
+        for (Resume r : arraySortedStorage.getAllSorted()) {
             System.out.println(r);
         }
     }
