0043e9108d2a6a1e3179363fdcf2082a41b2996e
young_vhr <blackrabbit154@gmail.com>
2025-03-02 14:52:22 +0700

    added tests

--- src/test/MapResumeStorageTest.java
+++ src/test/MapResumeStorageTest.java
@@ -1,2 +1,11 @@
-package test;public class MapResumeStorageTest {
+package test;
+
+import functional.AbstractStorage;
+import functional.StorageFactory;
+
+public class MapResumeStorageTest extends AbstractStorageTest{
+    @Override
+    AbstractStorage getStorage() {
+        return StorageFactory.createStorage(StorageFactory.StorageType.MAP_RESUME_STORAGE);
+    }
 }
--- src/test/MapUuidStorageTest.java
+++ src/test/MapUuidStorageTest.java
@@ -3,12 +3,12 @@ package test;
 import functional.AbstractStorage;
 import functional.StorageFactory;
 
-public class MapStorageTest extends AbstractStorageTest {
+public class MapUuidStorageTest extends AbstractStorageTest {
 
 
     @Override
     AbstractStorage getStorage() {
-        return StorageFactory.createStorage(StorageFactory.StorageType.MAP_STORAGE);
+        return StorageFactory.createStorage(StorageFactory.StorageType.MAP_UUID_STORAGE);
     }
 
 
